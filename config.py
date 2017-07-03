5f4af414e3f48cd9f60d27a1ed4dacdd2e7f815e
iLQ <danielcardeenas@gmail.com>
2017-07-03 19:26:57 +0000

    Before pull

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 credentials = dict(
-    phone = 'YOUR_PHONE',
-    password = 'YOUR_PASWWORD',
+    phone = '5218118478492',
+    password = '5c+7Yr5pCvJVUtN45ty3A+QEVkE=',
 )
 
 # Your contacts numbers (Framework will sync)
 # Add them here
 contacts = {
-    "CONTACT_PHONE": "CONTACT_NAME",
-    "ANOTHER_CONTACT_PHONE": "ANOTHER_CONTACT_NAME",
+    "5218114140740": "Daniel Cardenas",
+    "5218115112713": "Otniel",
+    "5218116559100": "Aaron de leon",
+    "5218111834751": "Kof",
+    "5218116133161": "Lucario",
+    "5218116616897": "Berni",
+    "5218114901244": "Casta",
+    "5218118419493": "Luis Muphu",
+    "5218123198861": "Idk"
 }
